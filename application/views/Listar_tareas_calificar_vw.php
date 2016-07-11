52a7f9cbcab32c681f76bc8e212ffa0b3bc5aca1
Alejandro Segura <alejandro.segura@codice.com>
2016-07-11 17:45:26 -0500

    CAmbios

--- application/views/Listar_tareas_calificar_vw.php
+++ application/views/Listar_tareas_calificar_vw.php
@@ -329,7 +329,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						</tbody>
 					</table>
 					<div id="statusChargeCalificados" class="progress-bar progress-bar-success progress-bar-striped active" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%">Cargando</div>
-					<div id="messagesCalificados">Demasiado información. Intente con un intervalo más pequeño.</div>
+					<h2 id="messagesCalificados">Demasiado información. Intente con un intervalo más pequeño.</h2>
 				</div>
 			</div>
 		</div>
