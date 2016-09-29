6dfaa4c421aeb14597c14ecd8fa065e9aac580e7
Alejandro Segura <alejandro.segura@codice.com>
2016-09-29 17:25:24 -0500

    Cambios

--- application/controllers/XLSReader_Master.php
+++ application/controllers/XLSReader_Master.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-ini_set('memory_limit','30M');
+ini_set('memory_limit','50M');
 
 class XLSReader_Master extends CI_Controller {
 
