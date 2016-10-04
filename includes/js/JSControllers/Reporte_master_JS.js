645726f48e4726d93e780bd8859635e8e953faab
Alejandro Segura <alejandro.segura@codice.com>
2016-10-04 15:53:54 -0500

    Master

---
+++ includes/js/JSControllers/Reporte_master_JS.js
@@ -0,0 +1,155 @@
+function retrieveData(){
+	var idCliente = $("#idCliente").val();
+	var idRazonSocial = $("#idRazonSocial").val();
+	var idCotizacion = $("#idCotizacion").val();
+	var table = $("#main-data-tbl tbody");
+
+	var fechaFacturaDesde = $("#fechaFacturaDesdeAlt").val();
+	var fechaFacturaHasta = $("#fechaFacturaHastaAlt").val();
+
+	var fechaPagoDesde = $("#fechaPagoDesdeAlt").val();
+	var fechaPagoHasta = $("#fechaPagoHastaAlt").val();
+
+	var fechaCancelacionDesde =$("#fechaCancelacionDesdeAlt").val();
+	var fechaCancelacionHasta = $("#fechaCancelacionHastaAlt").val();
+
+	var idEstadoFactura = $("#idEstadoFactura").val();
+
+	if(!($("#filterByFechaFactura").is(":checked"))){
+		fechaFacturaDesde = "none";
+		fechaFacturaHasta = "none";
+	}
+
+	if(!($("#filterByFechaPago").is(":checked"))){
+		fechaPagoDesde = "none";
+		fechaPagoHasta = "none";
+	}
+
+	if(!($("#filterByFechaCancelacion").is(":checked"))){
+		fechaCancelacionDesde = "none";
+		fechaCancelacionHasta = "none";
+	}
+
+	table.find("*").remove();
+
+	$.ajax({
+		url: baseURL+"index.php/Reporte_master_ctrl/getContentAJAX",
+		data: {"idCliente": idCliente, "idRazonSocial": idRazonSocial, "idCotizacion": idCotizacion, "facturaDesde": fechaFacturaDesde, "facturaHasta": fechaFacturaHasta, "pagoDesde": fechaPagoDesde, "pagoHasta": fechaPagoHasta, "cancelacionDesde": fechaCancelacionDesde, "cancelacionHasta": fechaCancelacionHasta, "idEstadoFactura": idEstadoFactura},
+		dataType: 'json',
+		method: 'post',
+		success: function(response){
+			var k, n, lastRow;
+
+			for(k=0, n=response.length; k<n; k++){
+				table.append("<tr></tr>");
+				lastRow = table.find("tr:last-child");
+
+				lastRow.append("<td>"+response[k].estadoFactura+"</td>");
+				lastRow.append("<td>"+response[k].folio+"</td>");
+				lastRow.append("<td>"+response[k].total+"</td>");
+				lastRow.append("<td>"+response[k].fechaPago+"</td>");
+				lastRow.append("<td>"+response[k].cliente+"</td>");
+				lastRow.append("<td>"+response[k].id+"</td>");
+				lastRow.append("<td>"+response[k].subtotal+"</td>");
+				lastRow.append("<td>"+response[k].moneda+"</td>");
+				lastRow.append("<td>"+response[k].fechaFactura+"</td>");
+				lastRow.append("<td>"+response[k].ordenCompra+"</td>");
+				lastRow.append("<td>"+response[k].tipoConcepto+"</td>");
+				lastRow.append("<td>"+response[k].referencia+"</td>");
+				lastRow.append("<td>"+response[k].descripcion+"</td>");
+				lastRow.append("<td>"+response[k].tituloCotizacion+"</td>");
+				lastRow.append("<td>"+response[k].fechaInicio+"</td>");
+				lastRow.append("<td>"+response[k].fechaFin+"</td>");
+				lastRow.append("<td>"+response[k].razonSocial+"</td>");
+				lastRow.append("<td>"+response[k].fechaVenta+"</td>");
+				lastRow.append("<td>"+response[k].fechaJuntaArranque+"</td>");
+				lastRow.append("<td>"+response[k].cerrador+"</td>");
+				lastRow.append("<td>"+response[k].responsable+"</td>");
+				lastRow.append("<td>"+response[k].accountManager+"</td>");
+				lastRow.append("<td>"+response[k].iva+"</td>");
+				lastRow.append("<td>"+response[k].montoIVA+"</td>");
+				lastRow.append("<td>"+response[k].importeEfectivo+"</td>");
+				lastRow.append("<td>"+response[k].fechaCancelacion+"</td>");
+				lastRow.append("<td>"+response[k].contrato+"</td>");
+				lastRow.append("<td>"+response[k].nota+"</td>");
+			}
+		},
+		error: function(){
+			alert("Ha ocurrido un error. Intente de nuevo, por favor.");
+		}
+	});
+}
+
+function retrieveRazonesSociales(){
+	var id = $("#idCliente").val();
+	var appendSection = $("#idRazonSocial");
+
+	appendSection.find("*").remove();
+	appendSection.append("<option value='-1'>Mostrar todas</option>")
+
+	$.ajax({
+		url: baseURL+'index.php/Reporte_master_ctrl/getRazonesSociales/',
+		dataType: 'json',
+		data: {"idCliente" : id},
+		method: 'post',
+		success: function(response){
+			var k, n;
+
+			for(k=0, n=response.length; k<n; k++){
+				appendSection.append("<option value="+response[k].id+">"+response[k].razonSocial+"</option>");
+			}
+		},
+		error: function(){
+			alert("Ha ocurrido un error. Intente de nuevo, por favor.");
+		}
+	});
+}
+
+function retrieveCotizaciones(){
+	var id = $("#idRazonSocial").val();
+	var appendSection = $("#idCotizacion");
+
+	appendSection.find("*").remove();
+	appendSection.append("<option value='-1'>Mostrar todas</option>")
+
+	$.ajax({
+		url: baseURL+'index.php/Reporte_master_ctrl/getCotizaciones/',
+		dataType: 'json',
+		data: {"idRazonSocial" : id},
+		method: 'post',
+		success: function(response){
+			var k, n;
+
+			for(k=0, n=response.length; k<n; k++){
+				appendSection.append("<option value="+response[k].id+">"+response[k].folio+"</option>");
+			}
+		},
+		error: function(){
+			alert("Ha ocurrido un error. Intente de nuevo, por favor.");
+		}
+	});
+}
+
+$(function(){
+	$("#btn-consultar").click(function(event){
+		event.preventDefault();
+
+		retrieveData();
+	});
+
+	$("#idCliente").change(function(){
+		retrieveRazonesSociales();
+	});
+
+	$("#idRazonSocial").change(function(){
+		retrieveCotizaciones();
+	});
+
+	initDatepicker("#fechaPagoDesde", "#fechaPagoDesdeAlt", "dd/mm/yy", "yy-mm-dd");
+	initDatepicker("#fechaCancelacionDesde", "#fechaCancelacionDesdeAlt", "dd/mm/yy", "yy-mm-dd");
+	initDatepicker("#fechaFacturaDesde", "#fechaFacturaDesdeAlt", "dd/mm/yy", "yy-mm-dd");
+
+	initDatepicker("#fechaPagoHasta", "#fechaPagoHastaAlt", "dd/mm/yy", "yy-mm-dd");
+	initDatepicker("#fechaCancelacionHasta", "#fechaCancelacionHastaAlt", "dd/mm/yy", "yy-mm-dd");
+	initDatepicker("#fechaFacturaHasta", "#fechaFacturaHastaAlt", "dd/mm/yy", "yy-mm-dd");
+});
