94fb46558002202c544bd386f27ff7c144958528
Alejandro Segura <alejandro.segura@codice.com>
2016-10-12 17:54:44 -0500

    c

--- application/models/XLSSheetDriver.php
+++ application/models/XLSSheetDriver.php
@@ -1,6 +1,7 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class XLSSheetDriver {
+class XLSSheetDriver extends CI_Model {
 	private $cCol;
 	private $cRow;
 	private $mark;
