8c697f440f05a9b080b16bf44fd3fedbbce1d176
Alejandro Segura <alejandro.segura@codice.com>
2017-02-13 17:42:16 -0600

    Cmbios

--- application/controllers/Alta_conceptos_cotizacion_ctrl.php
+++ application/controllers/Alta_conceptos_cotizacion_ctrl.php
@@ -15,8 +15,8 @@ class Alta_conceptos_cotizacion_ctrl extends CI_Controller {
 		$data['clientes'] = $this->db
 								 ->query("select * from catcliente where tipo = 0 and estadoActivo = 1")
 								 ->result();
-		$data['usuario'] = $this->db->query("select * from catusuario where idPuesto in (4,5) and estadoActivo = 1");
-		$data['account'] = $this->db->query("select * from catusuario where idPuesto in (4,5) and estadoActivo = 1");
+		$data['usuario'] = $this->db->query("select * from catusuario where idPuesto in (4,5) and activo = 'S'")->result();
+		$data['account'] = $this->db->query("select * from catusuario where idPuesto in (4,5) and activo = 'S'")->result();
 		$data['tipoConcepto'] = $this->db->query("select * from cattipoconcepto")->result();
 		$data['unidadMedida'] = $this->db->query("select * from catunidadmedida")->result();
 		$data['menu'] = $this->load->view("Menu_principal", null, true);
