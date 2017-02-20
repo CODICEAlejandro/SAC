c0ed608d5fc643fdeecab87c3879d2fa30153f18
Alejandro Segura <alejandro.segura@codice.com>
2017-02-20 16:13:49 -0600

    Cambios

--- application/controllers/Facturacion/Consulta_conceptos_facturacion_ctrl.php
+++ application/controllers/Facturacion/Consulta_conceptos_facturacion_ctrl.php
@@ -10,6 +10,8 @@ class Consulta_conceptos_facturacion_ctrl extends CI_Controller {
 	public function index(){
 		$data["facturacion"] = $this->cargar_principal(); 
 		$data["menu"] = $this->load->view("Menu_principal", null, true);
+
+		$this->load->view("Consulta_conceptos_facturacion_vw", $data);
 	}
 
 	//Carga la información principal 
