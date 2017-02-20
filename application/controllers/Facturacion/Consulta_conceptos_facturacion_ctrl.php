3a0d5d173116c4a4d08e6ca5a024b3a1fd8cf4e9
Alejandro Segura <alejandro.segura@codice.com>
2017-02-20 16:10:59 -0600

    Cambios

---
+++ application/controllers/Facturacion/Consulta_conceptos_facturacion_ctrl.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Alta_conceptos_cotizacion_ctrl extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function index(){
+		$data["facturacion"] = $this->cargar_principal(); 
+		$data["menu"] = $this->load->view("Menu_principal", null, true);
+	}
+
+	//Carga la información principal 
+	public function cargar_principal(){
+		$query = "
+					select 
+						hf.id id, 
+						c.nombre cliente, 
+						u.nombre account, 
+						hf.concepto concepto, 
+						hf.monto monto, 
+						DATE_FORMAT(hf.fecha,'%d/%m/%Y') fecha, 
+						if(hf.es_nuevo=1, 'NUEVO', if(hf.es_nuevo=2, 'PENDIENTE', 'ACTUAL')) temporalidad 
+					from historico_facturacion hf
+						inner join catcliente c on c.id = hf.id_cliente
+						inner join catusuario u on u.id = hf.id_account_manager
+					";
+
+		return $this->db->query($query)->result();
+	}
+}
+?>
---
+++ application/views/Facturacion/Consulta_conceptos_facturacion_vw.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php includeJQuery(); ?>
+	<?php includeBootstrap(); ?>
+
+	<script type="text/javascript">
+		var baseURL = "<?php echo base_url(); ?>";
+	</script>
+	<title>JOBS</title>
+
+	<style>
+		.btn-destroy-fecha {
+			cursor: pointer;
+		}
+
+		.btn-destroy-concepto {
+			cursor: pointer;
+		}
+	</style>
+
+</head>
+<body>
+	<?=$menu; ?>
+
+	<div class="container" style="margin-bottom: 20px; padding-bottom: 10px;">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<table class="table table-stripped table-hover">
+					<thead>
+						<th>Cliente</th>
+						<th>Account Manager</th>
+						<th>Concepto</th>
+						<th>Monto</th>
+						<th>Fecha</th>
+						<th>Temporalidad</th>
+					</thead>
+					<tbody>
+						<?php foreach($facturacion as $f){ ?>
+						<tr>
+							<td><?php echo $f->cliente; ?></td>
+							<td><?php echo $f->account; ?></td>
+							<td><?php echo $f->concepto; ?></td>
+							<td><?php echo $f->monto; ?></td>
+							<td><?php echo $f->fecha; ?></td>
+							<td><?php echo $f->temporalidad; ?></td>
+						</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</body>
