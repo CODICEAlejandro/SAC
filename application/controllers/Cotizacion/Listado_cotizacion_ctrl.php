ecc681ef556ff76bb10acb4c7ce2680f2b741ca9
Alejandro Segura <alejandro.segura@codice.com>
2017-04-03 12:59:21 -0500

    Cambios

---
+++ application/controllers/Cotizacion/Listado_cotizacion_ctrl.php
@@ -0,0 +1,50 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Listado_cotizacion_ctrl extends CI_Controller{
+	public function index(){
+<<<<<<< HEAD
+		$data["clientes"] = $this->db->query("select * from catcliente where estadoActivo=1 and tipo=0")->result();
+		$data["forma_pago"]= $this->db->query("select * from cat_tipo_cotizacion")->result();
+		$data["servicio_alcance"] = $this->db->query("select * from cattipoconcepto where tipo = 0")->result();
+		$data["cotizaciones"]= $this->db->query("SELECT cli.nombre nombre_cli, usu.nombre nombre_acc, usu.correo correo,cot.* 
+												FROM cotizacion cot
+												JOIN catcliente cli ON cot.idCliente = cli.id
+												JOIN catusuario usu ON cot.accountManager = usu.id
+												ORDER BY creacion DESC LIMIT 10")->result();
+		$data['menu'] = $this->load->view('Menu_principal',null,true);
+=======
+		$data['menu'] = $this->load->view('Menu_principal',null,true);
+		$data["cotizaciones"]= $this->db->query("SELECT cli.nombre nombre_cli, con.nombre nombre_acc, con.apellido apellido_acc, con.correo correo, sta.clave clave_status, cot.*, DATE_FORMAT(cot.fecha_alta, '%d-%m-%Y')  fecha_alta, DATE_FORMAT(cot.fecha_inicio_servicio, '%d-%m-%Y') fecha_inicio, DATE_FORMAT(cot.fecha_fin_servicio, '%d-%m-%Y') fecha_fin
+		FROM cotizacion_account cot
+		JOIN catcliente cli ON cot.id_cliente = cli.id
+		JOIN contacto con ON cot.id_contacto = con.id
+		JOIN cat_status_cotizacion sta ON cot.status_cotizacion_id=sta.id
+		ORDER BY cot.fecha_alta DESC LIMIT 20")->result();
+
+>>>>>>> a37d562e428362cc7d22da5679b58076af211308
+		$this->load->view("Cotizacion/Listado_cotizacion_vw", $data);
+	}
+
+	public function busquedaRegistros()
+	{
+		if (isset($_POST) && isset($_POST["parametro"])) {
+			$parametro = $_POST["parametro"];
+			$parametro = htmlentities($parametro,ENT_QUOTES,'UTF-8');
+
+			$cotizaciones= $this->db->query("
+				SELECT cli.nombre nombre_cli, con.nombre nombre_acc, con.apellido apellido_acc, con.correo correo, sta.clave clave_status, cot.*, DATE_FORMAT(cot.fecha_alta, '%d-%m-%Y')  fecha_alta, DATE_FORMAT(cot.fecha_inicio_servicio, '%d-%m-%Y') fecha_inicio, DATE_FORMAT(cot.fecha_fin_servicio, '%d-%m-%Y') fecha_fin
+				FROM cotizacion_account cot
+				JOIN catcliente cli ON cot.id_cliente = cli.id
+				JOIN contacto con ON cot.id_contacto = con.id
+				JOIN cat_status_cotizacion sta ON cot.status_cotizacion_id=sta.id
+				WHERE (cot.folio LIKE '%".$parametro."%'
+				OR cot.titulo LIKE '%".$parametro."%')
+				ORDER BY cot.fecha_alta DESC
+				")->result();
+
+			echo json_encode($cotizaciones);
+		}
+	}
+
+}
+?>
---
+++ application/views/Cotizacion/Listado_cotizacion_vw.php
@@ -0,0 +1,163 @@
+<?php  
+defined('BASEPATH') OR exit('No direct script access allowed'); 
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>JOBS</title>
+	<?php includeJQuery(); ?>
+	<?php includeBootstrap(); ?>
+
+	<script type="text/javascript">
+		var baseURL = "<?php echo base_url(); ?>";
+	</script>
+	<style>
+		.container{
+			width: 100% !important;
+			
+		}
+
+		.table{
+			text-align: center;
+			vertical-align: middle;
+		}
+	</style>
+	<script type="text/javascript" src="<?php echo base_url(); ?>includes/js/JSControllers/Cotizacion/Listado_cotizacion_JS.js"></script>
+</head>
+<body>
+	<?=$menu ?>
+
+	<div class="container">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<form class="form" action="Cotizacion/Tabla_cotizacion_ctrl.php" id="form_filtrado">
+				<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 form-group">
+					<label>Filtra los resultados por:</label>
+					<select class="form-control" name="filtrado" id="filtrado">
+						<option value="-1">Selecciona una opción</option>
+						<option value="1">Cliente</option>
+						<option value="2">Usuario</option>
+						<option value="3">Folio</option>
+						<option value="4">Status</option>
+						<option value="5">Fecha</option>
+						<option value="6">Vigentes/Vencidas</option>
+					</select>
+					<input type="submit" class="form-control btn btn-info" value="Filtrar">
+				</div>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<label>Buscar:</label>
+					<div class="form-group">
+					<input type="text" class="form-control" id="search" name="search" placeholder="Busca por título o folio">
+					<button class="btn btn-info form-control" id="btn-search">
+						<span class="glyphicon glyphicon-search"></span>
+					</button>
+					</div>
+				</div>
+			</form>
+			<table class="table table-bordered table-hover" id="tabla_cotizaciones">
+				<thead>
+					<th>Folio</th>
+					<th>Cliente</th>
+					<th>Título</th>
+					<th>Contacto</th>
+					<th>Importe Total</th>
+					<th>Fecha de alta</th>
+					<th>Fecha de inicio servicio</th>
+					<th>Fecha de término servicio</th>
+					<th>Usuario</th>
+					<th>Status</th>
+					<th>Exportar PDF</th>
+					<th>Editar</th>
+					<th>Aprobar</th>
+					<th>Cancelar</th>
+					<th>Duplicar</th>
+				</thead>
+				<tbody id="cuerpoTabla">
+					<?php foreach ($cotizaciones as $c){ ?>
+						<tr id="contenidoTabla">
+							<td><?php echo $c->folio; ?></td>
+							<td><?php echo $c->nombre_cli; ?></td>
+							<td><?php echo $c->titulo; ?></td>
+							<td><?php echo $c->nombre_acc." ".$c->apellido_acc."<br>".$c->correo; ?></td>
+							<td><?php echo $c->importe_total; ?></td>
+							<td><?php echo $c->fecha_alta; ?></td>
+							<td><?php echo $c->fecha_inicio; ?></td>
+							<td><?php echo $c->fecha_fin; ?></td>
+							<td><?php echo $c->nombre_acc; ?></td>
+							<td><?php echo $c->clave_status; ?></td>
+							<td>
+								<button class="btn btn-info">
+									<span class="glyphicon glyphicon-new-window"></span>
+								</button>
+							</td>
+							<td>
+								<button class="btn btn-warning">
+									<span class="glyphicon glyphicon-edit"></span>
+								</button>
+							</td>
+							<td>
+								<button class="btn btn-success">
+									<span class="glyphicon glyphicon-ok"></span>
+								</button>
+							</td>
+							<td>
+								<button class="btn btn-danger">
+									<span class="glyphicon glyphicon-remove"></span>
+								</button>
+							</td>
+							<td>
+								<button class="btn btn-primary">
+									<span class="glyphicon glyphicon-duplicate"></span>
+								</button>
+							</td>
+						</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+
+	
+<tr id="cloneSectionTabla" style="display: none;">
+	<td id="folio"></td>
+	<td id="nombreCliente"></td>
+	<td id="titulo"></td>
+	<td id="contacto"></td>
+	<td id="importe_total"></td>
+	<td id="fecha_alta"></td>
+	<td id="fecha_inicio"></td>
+	<td id="fecha_fin"></td>
+	<td id="nombreAcc"></td>
+	<td id="status"></td>
+	<td>
+		<button class="btn btn-info" style="display: none;">
+			<span class="glyphicon glyphicon-new-window"></span>
+		</button>
+	</td>
+	<td>
+		<button class="btn btn-warning" style="display: none;">
+			<span class="glyphicon glyphicon-edit"></span>
+		</button>
+	</td>
+	<td>
+		<button class="btn btn-success" style="display: none;">
+			<span class="glyphicon glyphicon-ok"></span>
+		</button>
+	</td>
+	<td>
+		<button class="btn btn-danger" style="display: none;">
+			<span class="glyphicon glyphicon-remove"></span>
+		</button>
+	</td>
+	<td>
+		<button class="btn btn-primary" style="display: none;">
+			<span class="glyphicon glyphicon-duplicate"></span>
+		</button>
+	</td>
+</tr>
+
+
+</body>
+</html>
