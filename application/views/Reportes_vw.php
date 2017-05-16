857d4956da039e340c74f559d2bc7d3827e2050e
Bernardo-Codice <bernardo@codice.com>
2017-05-16 15:29:49 -0500

    Se agrega seccion de reporte master proveedor

--- application/views/Reportes_vw.php
+++ application/views/Reportes_vw.php
@@ -21,7 +21,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<a class="btn list-group-item" href="<?php echo base_url().'index.php/Reporte_acumulado_tiempo_ctrl'; ?>" style="text-align: left;">Acumulado en tiempo</a>
 					<a class="btn list-group-item" href="<?php echo base_url().'index.php/Reporte_rentabilidad_ctrl'; ?>" style="text-align: left;">Reporte de rentabilidad</a>
 					<a class="btn list-group-item" href="<?php echo base_url().'index.php/Reporte_control_tareas_ctrl'; ?>" style="text-align: left;">Control de tareas</a>
-					<a class="btn list-group-item" href="<?php echo base_url().'index.php/Reporte_master_ctrl'; ?>" style="text-align: left;">Reporte master</a>
+					<a class="btn list-group-item" href="<?php echo base_url().'index.php/Reporte_master_ctrl'; ?>" style="text-align: left;">Reporte master cliente</a>
+					<a class="btn list-group-item" href="<?php echo base_url().'index.php/Reporte_master_proveedor_ctrl'; ?>" style="text-align: left;">Reporte master proveedor</a>
 					<a class="btn list-group-item" href="<?php echo base_url().'index.php/Facturacion/Reporte_facturacion_ctrl'; ?>" style="text-align: left;">Reporte de facturación</a>
 				</div>
 			</div>
