670f20be195d41b504a51b41eeb08f6a07143807
Bernardo-Codice <bernardo@codice.com>
2017-07-14 11:01:00 -0500

    Cambio en el importe mostrado en subir factura

--- application/views/Lectura_factura_proveedor_vw.php
+++ application/views/Lectura_factura_proveedor_vw.php
@@ -412,7 +412,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<label for="importeFactura">Importe</label>
-				<input type="text" id="importeFactura" class="form-control" style="margin-bottom: 15px;" value="<?php echo $generalFactura['subtotal']; ?>">
+				<input type="text" id="importeFactura" class="form-control" style="margin-bottom: 15px;" value="<?php echo $generalFactura['total']; ?>">
 			</div>
 		</div>
 
